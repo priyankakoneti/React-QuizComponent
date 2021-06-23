dfcaa0e8fbd01ee1d86762582455b0461973e1c8
Priyanka Koneti <priyanka.grpt49@gmail.com>
2021-06-23 12:43:40 +0200

    updated QuizEnd

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
 import QuizEnd from './QuizEnd.js';
+
 let quizData = require('./quiz_data.json');
+
 class Quiz extends Component
 {
     constructor(props)
@@ -11,16 +13,14 @@ class Quiz extends Component
     }
     render()
     {
-        const isQuizEnd = ((this.state.quiz_position-1) === quizData.quiz_questions.length)?true : false;
+        const isQuizEnd = ((this.state.quiz_position-1) === quizData.quiz_questions.length);
         return (
           <div>
               {/* /* <div className = "QuizQuestion">
               {quizData.quiz_questions[0].instruction_text}
               </div> */}
-              if(isQuizEnd)
-                <QuizEnd />
-              else
-                <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}/> 
+              {isQuizEnd ? <QuizEnd /> :
+                <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}/>} 
               
           </div>
         );
